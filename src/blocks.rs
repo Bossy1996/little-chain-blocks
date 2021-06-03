c3a438a2ec41f669651878deca9e74f325170f2d
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-03 17:46:23 +0200

    impl of block

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,18 +1,29 @@
+use std::fmt::{self, Debug, Formatter};
+use super::*;
+
 pub struct Block {
     pub index: u32,
     pub timestamp: u64,
     pub prev_block_hash: BlockHash,
     pub hash: BlockHash,
+    pub nonce: u64,
     pub payload: String,
 }
 
+/* impl Debug for Block {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block");
+    }
+} */
+
 impl Block {
-    pub fn new(index: u32, timestamp: u64, prev_block_hash: [u8; u16], payload: String) -> Self {
+    pub fn new(index: u32, timestamp: u64, prev_block_hash: BlockHash, nonce: u64,payload: String) -> Self {
         Block {
             index,
             timestamp,
             prev_block_hash,
-            hash: [0; 16],
+            hash: vec![0, 32],
+            nonce,
             payload,
         }
     }
