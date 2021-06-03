df9aa50283c0086a29c8ac35382ea841140b3e80
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-03 17:32:12 +0200

    Block struct

---
+++ src/blocks.rs
@@ -0,0 +1,19 @@
+pub Block {
+    pub index: u32,
+    pub timestamp: u64,
+    pub prev_block_hash: BlockHash,
+    pub hash: BlockHash,
+    pub payload: String,
+}
+
+impl Block {
+    pub fn new(index: u32, timestamp: u64, prev_block_hash: [u8; u16], payload: String) -> Self {
+        Block {
+            index,
+            timestamp,
+            prev_block_hash,
+            hash: [0; 16],
+            payload,
+        }
+    }
+}
